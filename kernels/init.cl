2eaccc3791f7e8a800dad888e77fa90ff008c620
unknown <synlo@outlook.com>
2020-10-15 23:15:57 +0200

    Add GL shader compilation

--- kernels/init.cl
+++ kernels/init.cl
@@ -2,9 +2,5 @@ __kernel void	initParticles(__global float3	*particles)
 {
 	int		gid = get_global_id(0);
 
-	float	x, y, z, n, r, oa, teta;
-	int		oa_index, teta_index, n_index;
-	int		i = gid;
-
 	particles[gid].xyz = (float3)(0.5, 0.5, 0.5);
 }
--- main.rb
+++ main.rb
@@ -16,8 +16,8 @@ GLFW.load_lib()
 GLU.load_lib()
 
 class OCL
-	attr_accessor :kernel, :queue, :context
-
+	attr_accessor :kernel, :queue, :context, :kernel_init
+	
 	def initialize(main, ogl)
 		@main = main
 		@ogl = ogl
@@ -31,53 +31,18 @@ class OCL
 		end
 
 		begin
-			@platform = OpenCL::platforms[0]
+			@platform 	= OpenCL.platforms.first
+			@device 	= @platform.devices.first
+			@context 	= OpenCL.create_context(@device, create_shared_context(@platform))
+			@queue 		= OpenCL.create_command_queue(@context, @device)
+			@prog 		= OpenCL.create_program_with_source(@context, init_kernel_src).build
 		rescue => e
-			puts("[OpenCL] Platform selection failed")
-			puts(e)
+			puts "[OpenCL] Error Code : %s" % [e]
+			begin puts(@prog.build_log) ; rescue ; end
 			exit()
 		end
 
-		begin
-			@device = @platform.devices[0]
-		rescue => e
-			puts("[OpenCL] Device selection failed")
-			puts(e)
-			exit()
-		end
-
-		begin
-			@context = OpenCL.create_context(@device, create_shared_context(@platform))
-		rescue => e
-			puts("[OpenCL] Context creation failed")
-			puts(e)
-			exit()
-		end
-
-		begin
-			@queue = @context.create_command_queue(@device, :properties => OpenCL::CommandQueue::PROFILING_ENABLE)
-		rescue => e
-			puts("[OpenCL] Command queue creation failed")
-			puts(e)
-			exit()
-		end
-
-		begin
-			@prog = @context.create_program_with_source( init_kernel_src )
-		rescue => e
-			puts("[OpenCL] Program creation failed")
-			puts(e)
-			exit()
-		end
-
-		begin
-			@kernel = @prog.build
-		rescue => e
-			puts("[OpenCL] Build failed")
-			puts(e)
-			puts(@prog.build_log)
-			exit()
-		end
+		@kernel_init = OpenCL.create_kernel(@prog, "initParticles")
 
 		begin
 			@cl_buff = OpenCL.create_from_gl_buffer(@context, ogl.mesh_vbo[0])
@@ -87,16 +52,11 @@ class OCL
 			exit()
 		end
 
-		begin
-			OpenCL.enqueue_acquire_gl_objects(@queue, @cl_buff)
-		rescue => e
-			puts("[OpenCL] Acquire GL Buffer failed")
-			puts(e)
-			exit()
-		end
-		OpenCL.enqueue_release_gl_objects(@queue, @cl_buff)
-
-		OpenCL.finish(@queue)
+		#OpenCL.enqueue_acquire_gl_objects(@queue, @cl_buff)
+		#OpenCL.set_kernel_arg(@kernel_init, 0, @cl_buff)
+		#OpenCL.enqueue_ndrange_kernel(@queue, @kernel_init, [(main.count / 100).round], :local_work_size => [100])
+		#OpenCL.enqueue_release_gl_objects(@queue, @cl_buff)
+		#OpenCL.finish(@queue)
 	end
 
 
@@ -120,8 +80,8 @@ class Main
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
 
-		@window = glfwCreateWindow( width, height, "Particles System", nil, nil )
-		glfwMakeContextCurrent( window )
+		@window = glfwCreateWindow(width, height, "Particles System", nil, nil)
+		glfwMakeContextCurrent(window)
 		ratio = width.to_f / height.to_f
 		glViewport(0, 0, 1280, 720)
 		glMatrixMode(GL_PROJECTION)
@@ -134,10 +94,9 @@ class Main
 		glfwShowWindow(@window)
 		while glfwWindowShouldClose( window ) == 0
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-			#glLoadIdentity()
-			#glTranslatef(0.0, 0.0, -50.0)
-			#glRotatef(glfwGetTime() * 50.0, 0.0, 1.0, 0.0)
-			#teapot.render
+			glLoadIdentity()
+			glTranslatef(0.0, 0.0, -50.0)
+			glRotatef(glfwGetTime() * 50.0, 0.0, 1.0, 0.0)
 			glfwSwapBuffers( window )
 			glfwPollEvents()
 		end
@@ -159,7 +118,48 @@ class OGL
 		#glBindBuffer(GL_ARRAY_BUFFER, 0)
 		glEnableVertexAttribArray(0)
 		glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, 0, nil)
-		
+		self.init_programs()
+	end
+
+	def create_shader(src, type)
+		shader = glCreateShader(type)
+		glShaderSource(shader, 1, [src].pack('p'), [src.size].pack('I'))
+		glCompileShader(shader)
+
+		ret_value_buffer = ' ' * 4
+		glGetShaderiv(shader, GL_COMPILE_STATUS, ret_value_buffer);
+		ret_value = ret_value_buffer.unpack('L')[0]
+
+		if ret_value == 0
+			puts "Error in compiling shader"
+			exit()
+		end
+		return (shader)
+	end
+
+	def init_programs()
+		vertex_file = "kernels/vertex.glsl"
+		fragment_file = "kernels/fragment.glsl"
+
+
+		begin
+			vertex_src = File.read(vertex_file)
+		rescue
+			puts "Can't open %s" % [vertex_file]
+			exit()
+		end
+		begin
+			fragment_src = File.read(fragment_file)
+		rescue
+			puts "Can't open %s" % [fragment_file]
+			exit()
+		end
+
+		@program = glCreateProgram()
+
+		vertex_shader = self.create_shader(vertex_src, GL_VERTEX_SHADER)
+		fragment_shader = self.create_shader(fragment_src, GL_FRAGMENT_SHADER)
+		exit()
 	end
 end
 
