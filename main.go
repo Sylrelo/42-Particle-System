37beadbd52ced5edc7115f1ed10fb1e5270941c3
Synlo <synlo@outlook.com>
2024-11-10 00:00:05 +0100

    Testing Orbit Camera (need to move to different file)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"runtime"
 
@@ -11,9 +12,161 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+const (
+	MAX_CAM_SMOOTH = 100
+)
+
 func init() {
 	runtime.LockOSThread()
 }
+
+type OrbitCamera struct {
+	Position     mgl32.Vec3
+	Target       mgl32.Vec3
+	TargetOffset mgl32.Vec3
+	Up           mgl32.Vec3
+	Azimuth      float32
+	Elevation    float32
+	Distance     float32
+
+	PanDelta    mgl32.Vec3
+	RotateDelta mgl32.Vec3
+}
+
+type InputData struct {
+	mouseButton [8]glfw.Action
+	_oldMouseX  float64
+	_oldMouseY  float64
+}
+
+type ParticleSystem struct {
+	windowHeight int
+	windowWidth  int
+
+	cameraMatrix mgl32.Mat4
+	orbitCamera  OrbitCamera
+
+	inputs InputData
+}
+
+func (ps *ParticleSystem) SmoothMovement() {
+
+	if abs(ps.orbitCamera.RotateDelta[0]) > 0.1 || abs(ps.orbitCamera.RotateDelta[1]) > 0.1 {
+		valX := ps.orbitCamera.RotateDelta[0] * 0.05
+		valY := ps.orbitCamera.RotateDelta[1] * 0.05
+
+		ps.orbitCamera.RotateDelta[0] -= valX
+		ps.orbitCamera.RotateDelta[1] -= valY
+
+		ps.HandleCameraRotation(valX, valY, false)
+	}
+
+	if abs(ps.orbitCamera.PanDelta[0]) > 0.1 || abs(ps.orbitCamera.PanDelta[1]) > 0.1 {
+		tmp := ps.orbitCamera.PanDelta.Mul(0.05)
+
+		ps.orbitCamera.PanDelta = ps.orbitCamera.PanDelta.Sub(tmp)
+
+		ps.HandleCameraMovement(tmp[0], tmp[1], false)
+	}
+}
+
+func (ps *ParticleSystem) RecalculateCamera() {
+	azimuthRad := ps.orbitCamera.Azimuth * float32(math.Pi) / 180.0
+	elevationRad := ps.orbitCamera.Elevation * float32(math.Pi) / 180.0
+
+	xOffset := ps.orbitCamera.Distance * float32(math.Cos(float64(elevationRad))) * float32(math.Sin(float64(azimuthRad)))
+	yOffset := ps.orbitCamera.Distance * float32(math.Sin(float64(elevationRad)))
+	zOffset := ps.orbitCamera.Distance * float32(math.Cos(float64(elevationRad))) * float32(math.Cos(float64(azimuthRad)))
+
+	ps.orbitCamera.Position = mgl32.Vec3{
+		ps.orbitCamera.Target.X() + ps.orbitCamera.TargetOffset[0] + xOffset,
+		ps.orbitCamera.Target.Y() + ps.orbitCamera.TargetOffset[1] + yOffset,
+		ps.orbitCamera.Target.Z() + ps.orbitCamera.TargetOffset[2] + zOffset,
+	}
+
+	actualTarget := ps.orbitCamera.Target.Add(ps.orbitCamera.TargetOffset)
+
+	ps.cameraMatrix = mgl32.LookAtV(
+		ps.orbitCamera.Position,
+		actualTarget,
+		mgl32.Vec3{0, 1, 0},
+	)
+}
+
+func (ps *ParticleSystem) HandleCameraRotation(x float32, y float32, storeDelta bool) {
+	ps.orbitCamera.Azimuth += x
+	ps.orbitCamera.Elevation += y
+
+	if storeDelta {
+		ps.orbitCamera.RotateDelta = clampvec3n(
+			ps.orbitCamera.RotateDelta.Add(mgl32.Vec3{x, y, 0}),
+			0,
+			MAX_CAM_SMOOTH,
+		)
+	}
+
+	ps.RecalculateCamera()
+}
+
+func (ps *ParticleSystem) HandleCameraMovement(x float32, y float32, storeDelta bool) {
+	forward := ps.orbitCamera.Target.
+		Add(ps.orbitCamera.TargetOffset).
+		Sub(ps.orbitCamera.Position).
+		Normalize()
+
+	right := forward.
+		Cross(ps.orbitCamera.Up).
+		Normalize()
+
+	up := right.
+		Cross(forward).
+		Normalize()
+
+	ps.orbitCamera.TargetOffset = ps.orbitCamera.TargetOffset.Add(right.Mul(x * 0.1)).Add(up.Mul(y * 0.1))
+
+	if storeDelta {
+		ps.orbitCamera.PanDelta = clampvec3n(
+			ps.orbitCamera.PanDelta.Add(mgl32.Vec3{x, y, 0}),
+			0,
+			MAX_CAM_SMOOTH,
+		)
+	}
+
+	ps.RecalculateCamera()
+}
+
+func (inp *ParticleSystem) MouseButtonEvent(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
+	inp.inputs.mouseButton[button] = action
+}
+
+func (ps *ParticleSystem) MouseMotionEvent(w *glfw.Window, xpos float64, ypos float64) {
+	diffX := (ps.inputs._oldMouseX - xpos) * 0.1
+	diffY := (ps.inputs._oldMouseY - ypos) * 0.1
+	ps.inputs._oldMouseX = xpos
+	ps.inputs._oldMouseY = ypos
+
+	if ps.inputs.mouseButton[glfw.MouseButtonLeft] == glfw.Press {
+		ps.HandleCameraRotation(float32(diffX), float32(diffY), true)
+	}
+
+	if ps.inputs.mouseButton[glfw.MouseButtonRight] == glfw.Press {
+		ps.HandleCameraMovement(float32(diffX), float32(diffY), true)
+	}
+
+}
+
+func (ps *ParticleSystem) MouseScrollEvent(w *glfw.Window, xoff float64, yoff float64) {
+	ps.orbitCamera.Distance += float32(xoff)
+	ps.orbitCamera.Position[2] += float32(xoff)
+	ps.RecalculateCamera()
+}
+
+func (inp *ParticleSystem) WindowResizeEvent(w *glfw.Window, width int, height int) {
+	inp.windowHeight = height
+	inp.windowWidth = width
+}
+
+///
 
 func main() {
 	if err := glfw.Init(); err != nil {
@@ -21,18 +174,34 @@ func main() {
 	}
 	defer glfw.Terminate()
 
+	system := ParticleSystem{}
+
+	system.orbitCamera = OrbitCamera{
+		Position:     mgl32.Vec3{0, 0, 10},
+		Target:       mgl32.Vec3{0, 0, 0},
+		TargetOffset: mgl32.Vec3{0, 0, 0},
+		Up:           mgl32.Vec3{0, 1, 0},
+		Azimuth:      0,
+		Elevation:    0,
+		Distance:     10,
+	}
+	system.RecalculateCamera()
+
 	glfw.WindowHint(glfw.ContextVersionMajor, 4)
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	// glfw.WindowHint(glfw.AlphaBits, 8)
 
 	window, err := glfw.CreateWindow(1280, 720, "OpenCL-OpenGL Interop", nil, nil)
 	if err != nil {
 		log.Fatalln("failed to create glfw window:", err)
 	}
 	window.MakeContextCurrent()
+
+	window.SetSizeCallback(system.WindowResizeEvent)
+	window.SetCursorPosCallback(system.MouseMotionEvent)
+	window.SetMouseButtonCallback(system.MouseButtonEvent)
+	window.SetScrollCallback(system.MouseScrollEvent)
 
 	if err := gl.Init(); err != nil {
 		log.Fatalln("failed to initialize gl:", err)
@@ -73,7 +242,6 @@ func main() {
 
 	////////////////////////
 	perspectiveMatrix := mgl32.Perspective(mgl32.DegToRad(45.0), 16.0/9.0, 0.01, 10000)
-	cameraMatrix := mgl32.LookAtV(mgl32.Vec3{0, 0, 10}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
 	// cameraMatrix := mgl32.Ident4()
 
 	// perspectiveMatrix = mgl32.Ident4()
@@ -83,7 +251,6 @@ func main() {
 
 	gl.UseProgram(program)
 	gl.UniformMatrix4fv(perspectiveUniform, 1, false, &perspectiveMatrix[0])
-	gl.UniformMatrix4fv(cameraUniform, 1, false, &cameraMatrix[0])
 
 	cl_compute.RunInitKernel(particlesCount)
 
@@ -110,11 +277,16 @@ func main() {
 	gl.ClearColor(0, 0, 0, 1.0)
 
 	for !window.ShouldClose() {
+		gl.UniformMatrix4fv(cameraUniform, 1, false, &system.cameraMatrix[0])
+
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		gl.Finish()
 
 		cl_compute.RunGravitateKernel(particlesCount)
+
+		system.SmoothMovement()
+
 		// gl.UseProgram(program)
 		// gl.EnableVertexAttribArray(0)
 		// gl.VertexAttribPointer(0, 4, gl.FLOAT, false, 0, nil)
@@ -122,10 +294,6 @@ func main() {
 		// gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 		gl.BindVertexArray(vao)
 		gl.DrawArrays(gl.POINTS, 0, int32(particlesCount))
-
-		// clQueue.AcquireGLObjects((clgo.CL_MEM)(unsafe.Pointer(clBuffer)))
-		// clQueue.ReleaseGLObjects((clgo.CL_MEM)(unsafe.Pointer(clBuffer)))
-		// clQueue.Finish()
 
 		gl.BindBuffer(gl.ARRAY_BUFFER, 0)
 
--- utils.go
+++ utils.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/go-gl/mathgl/mgl32"
+)
 
 func ExitOnError(err error) {
 	if err == nil {
@@ -8,4 +12,41 @@ func ExitOnError(err error) {
 	}
 
 	log.Fatalln(err)
+}
+
+func abs(in float32) float32 {
+	if in < 0 {
+		return in * -1
+	}
+
+	return in
+}
+
+func clamp(value float32, max float32, min float32) float32 {
+	if value > max {
+		return max
+	}
+
+	if value < min {
+		return min
+	}
+
+	return value
+}
+
+func clampn(value float32, max float32, min float32) float32 {
+	sign := 1.0
+	if value < 0 {
+		sign = -1.0
+	}
+
+	return clamp(abs(value), max, min) * float32(sign)
+}
+
+func clampvec3n(v mgl32.Vec3, min float32, max float32) mgl32.Vec3 {
+	v[0] = clampn(v[0], max, min)
+	v[1] = clampn(v[1], max, min)
+	v[2] = clampn(v[2], max, min)
+
+	return v
 }
